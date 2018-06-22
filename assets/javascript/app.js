e477a812f2fc96b2494231c18f24b3a039292caf
1661sons <julia.kerouac@gmail.com>
2018-06-22 19:56:17 -0400

    done pseudo-coding .. i think

---
+++ assets/javascript/app.js
@@ -0,0 +1,38 @@
+// API key = nrnOysmPzPBWjJYThMdlRdiJzBtuJEJp
+$(document).ready( function(){
+
+    // api params to use
+        // q, limit, and rating
+    // make sure the query URL is using https
+    // create a variable called topics to store an array of strings - the strings should relate to a topic of my choosing
+        // topics: black panther, muhammad ali, falcons, hawks, braves, grand theft auto san andreas, jeep wranglers
+        var topics = ['black panther', 'muhammad ali', 'atlanta falcons', 'atlanta hawks', 'atlanta braves', 'grand theft auto san andreas', 'jeep wrangler'];
+
+    // create a button for each topic in the array and append the button to the DOM
+        // when this button is clicked, GET the search query of the topic and respond with 10 gifs related to the topic - prepend these 10 gifs on the page
+        // these gifs should be still on default but animate when clicked, then still when clicked again (toggle)
+        // display the gif's rating under each one
+
+    // allow the user to add their own topic button to append 10 gifs of their topic choice to the page (maybe prepend?)
+
+    // BONUS // allow the user to download any gif with a one-click button
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// closing ready function below
+});
