be8da199ff56f36a14fb7bc5776b11e578f1e27f
1661sons <julia.kerouac@gmail.com>
2018-06-24 16:14:06 -0400

    finally able to get the button's text value on click and save it as variable for search query

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,31 +1,23 @@
 // API key = nrnOysmPzPBWjJYThMdlRdiJzBtuJEJp
-$(document).ready( function(){
+$(document).ready( function() {
 
     // api params to use
         // q, limit, and rating
     // make sure the query URL is using https
     // create a variable called topics to store an array of strings
-        var topics = ['black panther', 'muhammad ali', 'atlanta falcons', 'atlanta hawks', 'atlanta braves', 'grand theft auto san andreas', 'jeep wrangler'];
+    var topics = ['black panther', 'muhammad ali', 'atlanta falcons', 'atlanta hawks', 'atlanta braves', 'grand theft auto san andreas', 'jeep wrangler'];
 
-        $.each(topics, function(index, value) {
-            // for each topic in the topics array, create a button with its corresponding value as the button text
-            $('.gifDiv').append('<button data=' + value + ' class="btn btn-outline-dark">' + value);
-            // $('.btn').attr('data', value);
-        });
-
-        // $(this).on('click', function(){
-        //     var topic = 
-        // });
-
-
-
-
-
-
-
-            
-
+    // for each topic in the topics array, 
+    $.each(topics, function(index, value) {
+        // create a button with its corresponding value as the button text
+        $('.gifDiv').append('<button data=' + index + ' class="btn btn-outline-dark">' + value);
+    });
     
+    // when a button is clicked
+    $('.btn').on('click', function() {
+        // grab the button's text and save it as a variable called topic
+        var topic = $(this).text();
+        console.log(topic);
 
         var queryURL = 'https://api.giphy.com/v1/gifs/search?api_key=nrnOysmPzPBWjJYThMdlRdiJzBtuJEJp&q=&limit=10&offset=0&rating=PG-13&lang=en'
 
@@ -42,7 +34,6 @@ $(document).ready( function(){
         // closing then function below
         });
 
-    // create a button for each topic in the array and append the button to the DOM
         // when this button is clicked, GET the search query of the topic and respond with 10 gifs related to the topic - prepend these 10 gifs on the page
         // these gifs should be still on default but animate when clicked, then still when clicked again (toggle)
         // display the gif's rating under each one
@@ -50,6 +41,9 @@ $(document).ready( function(){
     // allow the user to add their own topic button to append 10 gifs of their topic choice to the page (maybe prepend?)
 
     // BONUS // allow the user to download any gif with a one-click button
+
+    // closing button click even handler below   
+    });
 
 
 
